158a6e01b2d98cf0a12168b025223bd567dc4d2c
Sai Oduri <odurisai@gmail.com>
2020-12-01 18:44:52 -0500

    ✨ Completed Day1 Part 2

--- day1.py
+++ day1.py
@@ -1,11 +1,34 @@
-def day_one(f):
+def transformFileIntoArray(f):
     fp = open(f)
-    two_sum = {}
+    array = []
     for line_string in fp:
+        array.append(int(line_string))
+    return array
+
+def day_one_part1(f):
+    two_sum = {}
+    array = transformFileIntoArray(f)
+    for line_string in array:
         line = int(line_string)
         difference = 2020-line
         if(difference in two_sum.keys()):
             return(difference*line)
         else:
             two_sum[line] = difference
+    return 0
+
+def day_one_part2(f):
+    A = transformFileIntoArray(f)
+    A.sort()
+    for i in range(0, len(A)-2):
+        l = i+1
+        r = len(A)-1
+        while(l < r):
+            total = A[i] + A[l] + A[r]
+            if (total == 2020):
+                return(A[i]*A[l]*A[r])
+            elif(total < 2020):
+                l += 1
+            else:
+                r -= 1
     return 0
